4fe56fc0bf01f0084c2e9b6906ee629bad3a523e
Mohammed Elalimy <mohammed.m.elalimy@gmail.com>
2026-03-08 23:42:37 +0200

    fix: rename Compoentst to components and update imports

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,42 +1,81 @@
-import { createBrowserRouter } from "react-router-dom"
-import Auth from "./Layouts/Auth"
-import MainLayout from "./Layouts/MainLayout"
-import Brands from "./Pages/Brands/Brands"
-import Cart from "./Pages/Cart/Cart"
-import Categories from "./Pages/Categories/Categories"
-import Details from "./Pages/Details/Details"
-import Home from "./Pages/Home/Home"
-import Login from "./Pages/Login/Login"
-import Products from "./Pages/Products/Products"
-import Register from "./Pages/Register/Register"
-import ProtectedRoute from "/src/Components/Protected/ProtectedRoute"
-import Payment from "./Pages/Payment/Payment"
-
-
+import { createBrowserRouter } from 'react-router-dom';
+import Auth from './Layouts/Auth';
+import MainLayout from './Layouts/MainLayout';
+import Brands from './Pages/Brands/Brands';
+import Cart from './Pages/Cart/Cart';
+import Categories from './Pages/Categories/Categories';
+import Details from './Pages/Details/Details';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Products from './Pages/Products/Products';
+import Register from './Pages/Register/Register';
+import ProtectedRoute from '/src/components/Protected/ProtectedRoute';
+import Payment from './Pages/Payment/Payment';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <MainLayout/>,
-    children:[
-      {index:true , element:<Home/>},
-      {path:'register' , element:<Register/>},
-      {path:'login' , element:<Login/>},
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'register', element: <Register /> },
+      { path: 'login', element: <Login /> }
     ]
   },
   {
-
-    path:"/authUser",
-    element:<Auth/>,
-    children:[
-      {index:true , element:<Home/>},
-      {path:'products' , element:<ProtectedRoute><Products/></ProtectedRoute>},
-      {path:'categories' , element:<ProtectedRoute><Categories/></ProtectedRoute>},
-      {path:'brands' , element:<ProtectedRoute><Brands/></ProtectedRoute>},
-      {path:'cart' , element:<ProtectedRoute><Cart/></ProtectedRoute>},
-      {path:'details/:id' , element:<ProtectedRoute><Details/></ProtectedRoute>},
-      {path:'payment' , element:<ProtectedRoute><Payment/></ProtectedRoute>},
+    path: '/authUser',
+    element: <Auth />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: 'products',
+        element: (
+          <ProtectedRoute>
+            <Products />
+          </ProtectedRoute>
+        )
+      },
+      {
+        path: 'categories',
+        element: (
+          <ProtectedRoute>
+            <Categories />
+          </ProtectedRoute>
+        )
+      },
+      {
+        path: 'brands',
+        element: (
+          <ProtectedRoute>
+            <Brands />
+          </ProtectedRoute>
+        )
+      },
+      {
+        path: 'cart',
+        element: (
+          <ProtectedRoute>
+            <Cart />
+          </ProtectedRoute>
+        )
+      },
+      {
+        path: 'details/:id',
+        element: (
+          <ProtectedRoute>
+            <Details />
+          </ProtectedRoute>
+        )
+      },
+      {
+        path: 'payment',
+        element: (
+          <ProtectedRoute>
+            <Payment />
+          </ProtectedRoute>
+        )
+      }
     ]
-}
-])
-export default router
+  }
+]);
+export default router;
